83107e87d3721cfdaacb9318bc0ef10ce24f35e2
Thrivikram-Rakesh <thrivikramrakesh@gmail.com>
2024-06-06 22:16:01 +0530

    second commit

--- coreJava/src/com/coreJava/practice/Practice1.java
+++ coreJava/src/com/coreJava/practice/Practice1.java
@@ -1,9 +1,17 @@
 package com.coreJava.practice;
+import java.util.*;
 
 public class Practice1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		sc.nextLine();
+		String name = sc.nextLine();
+		float cgpa = sc.nextFloat();
+		System.out.println(num+ " \n"+name+" \n"+cgpa+" \n");
+		sc.close();
+		
 	}
 
 }
